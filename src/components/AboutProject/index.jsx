3757ae0ea6349ca3405d176674b89aa58d667ca5
Driadix <59824288+Driadix@users.noreply.github.com>
2023-06-13 02:50:38 +0300

    feat: add adaptive to promo block

--- src/components/AboutProject/index.jsx
+++ src/components/AboutProject/index.jsx
@@ -3,8 +3,31 @@ import React from 'react'
 const AboutProject = () => {
   return (
     <section className="about-project">
-        <h2 className="about-project__subtitle">О проекте</h2>
-
+        <h2 className="about-project__title">О проекте</h2>
+        <div className="about-project__container">
+          <div className="about-project__info-area">
+          <div className="about-project__info-box">
+            <h3 className="about-project__info-title">Дипломный проект включал 5 этапов</h3>
+            <p className="about-project__info-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          </div>
+          <div className="about-project__info-box">
+            <h3 className="about-project__info-title">На выполнение диплома ушло 5 недель</h3>
+            <p className="about-project__info-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          </div>
+          </div>
+          <div className="about-project__schedule">
+            <div className="about-project__schedule-container">
+              <div className="about-project__schedule-time">
+                <p className="about-project__schedule-time-text">1 неделя</p>
+                <p className="about-project__schedule-time-subtext">Back-end</p>
+              </div>
+              <div className="about-project__schedule-time">
+                <p className="about-project__schedule-time-text">4 недели</p>
+                <p className="about-project__schedule-time-subtext">Front-end</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
   )
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -4,16 +4,19 @@ import AboutProject from '../AboutProject';
 import Techs from '../Techs';
 import AboutMe from '../AboutMe';
 import Portfolio from '../Portfolio';
+import Container from '../Container';
 import './styles.scss';
 
 const Main = () => {
   return (
     <main className="intro">
       <Promo />
+      <Container>
       <AboutProject />
       <Techs />
       <AboutMe />
       <Portfolio />
+      </Container>
     </main>
   )
 }
