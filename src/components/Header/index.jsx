9fea9e079358cd4fe2742ea7e6873f52d217d2ab
Driadix <59824288+Driadix@users.noreply.github.com>
2023-06-14 00:47:32 +0300

    feat: add base auth header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../images/logo.svg'
 import Container from '../Container';
+import ProfilePicture from '../../images/profile-icon.svg'
 import './styles.scss'
 
 const Header = ({isIntroPage = false}) => {
+  const navigate = useNavigate();
+
   return (
     
     <header className={`header ${isIntroPage && 'header_theme_gray'}`}>
       <Container>
       <div className="header__flex-container">
       <img src={logo} alt="Site logo" className="header__logo" />
-      <div className="header__content">
-        {isIntroPage && 
-          (<>
-          <Link to='signup' className="header__register-button">Регистрация</Link>
-          <Link to='signin' className="header__login-button">Войти</Link>
-          </>
+      <nav className="header__content">
+        {isIntroPage ?
+          (
+          <ul className="header__list">
+            <li className="header__item"><Link to='signup' className="header__register-button">Регистрация</Link></li>
+            <li className="header__item"><Link to='signin' className="header__login-button">Войти</Link></li>
+          </ul>
+          ):(
+          <ul className="header__list">
+            <div className="header__movies-links">
+              <li className="header__item"><Link to='movies' className="header__movies-button">Фильмы</Link></li>
+              <li className="header__item"><Link to='saved-movies' className="header__saved-movies-button">Сохранённые фильмы</Link></li>
+            </div>
+              <li onClick={() => navigate('/profile')} className="header__item header__item_account">
+                <a href="#" className="header__profile-button">Аккаунт</a>
+                <img src={ProfilePicture} alt="profile icon" className="header__profile-icon" />
+              </li>
+          </ul>
           )
         }
-        </div>
+        </nav>
       </div>
       </Container>
     </header>
