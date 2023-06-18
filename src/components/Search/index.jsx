055099d5992ed5165ddfae08d3a9e7f8f556cc59
Driadix <59824288+Driadix@users.noreply.github.com>
2023-06-18 16:41:34 +0300

    fix: add focus to inputs, change lang, check val, change search to be form

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -7,13 +7,13 @@ const Search = () => {
   return (
     <div className="search">
       <div className="search__container">
-        <div className="search__main">
+        <form className="search__main">
           <div className="search__input-container">
-            <img src={searchIcon} alt="input icon" className="search__input-icon" />
-            <input type="text" placeholder='Фильмы' className="search__input" />
+            <img src={searchIcon} alt="input icon" className="search__input-icon"/>
+            <input type="text" placeholder='Фильмы' className="search__input" required/>
           </div>
-          <button className="search__input-button"></button>
-        </div>
+          <button type="submit" className="search__input-button"></button>
+        </form>
         <div className="search__toggle">
           <FilterCheckbox />
           <p className="search__toggle-name">Короткометражки</p>
